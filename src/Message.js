0ff68d5d53373a6feed8bc432bdc84d52cce42bf
eetosuu <77057144+eetosuu@users.noreply.github.com>
2022-04-02 14:46:48 +0300

    less-2

--- src/Message.js
+++ src/Message.js
@@ -1,5 +1,8 @@
-const Message = (props) => {
-    return <div className={"main"}><div className={"hiText"} >Hello, {props.name}!</div></div>
+const Message = ({text}) => {
+    return <div className={"message"}>
+        <div className={"message-author"} >{text.author}</div>
+        <div className={"message-text"} >{text.text}</div>
+    </div>
 };
 
 export default Message;
